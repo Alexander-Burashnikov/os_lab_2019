a24fe80f1a1cc3186416b511132833d704a041f8
Alexander <62155333+Alexander-Burashnikov@users.noreply.github.com>
2021-06-25 13:09:14 +0300

    Update process_memory.c

--- lab4/src/process_memory.c
+++ lab4/src/process_memory.c
@@ -1,4 +1,3 @@
-
 /* Program to display address information about the process */
 /* Adapted from Gray, J., program 1.4 */
 #include <stdio.h>
@@ -17,7 +16,7 @@ char *cptr = "This message is output by the function showit()\n"; /* Static */
 char buffer1[25];
 int showit(); /* Function prototype */
 
-main() {
+int main() {
   int i = 0; /* Automatic variable */
 
   /* Printing addressing information */
